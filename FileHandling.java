144923425027e9f03c49bde5d2ac5477abe1603e
Abhinav Kumar Jha <akj0712@duck.com>
2022-02-09 15:31:16 +0530

    Initial Commit

--- FileHandling.java
+++ FileHandling.java
@@ -1,9 +1,24 @@
-//import java.io.*;
-//
-//public class FileHandling {
-//	public static void main(String[] args) throws IOException, FileNotFoundException{
-//		char ch;
-//		int i;
-//		FileInputStream fin = newFileInputStream ("FileHandling.txt");
-//	}
-//}
+import java.io.*;
+
+public class FileHandling {
+	public static void main(String[] args) throws IOException, FileNotFoundException{
+		char c;
+		int i;
+		FileInputStream fin = new FileInputStream ("E:\\Study Material\\Eclipse-Workspace\\Lab-Manual\\src\\FileHandling.txt");
+		FileOutputStream fout = new FileOutputStream ("E:\\\\Study Material\\\\Eclipse-Workspace\\\\Lab-Manual\\\\src\\\\FileHandling.txt"); //Set path of Test.txt
+		BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+		System.out.println ("Enter the characters: q to quit");
+		do {
+		c = (char)br.read();
+		if (c != 'q')
+		fout.write(c);
+		} while (c != 'q');
+		do {
+		i = fin.read();
+		if (i != -1)
+		System.out.print ((char)i);
+		} while (i != -1);
+		fin.close();
+		fout.close();
+	}
+}
